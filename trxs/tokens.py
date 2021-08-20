dd90cea6c537e378847bfa6db88813b75488759d
Marcelo Poblete <marcelopoblette@gmail.com>
2021-08-19 22:17:35 -0400

    how to put your tokens

--- trxs/tokens.py
+++ trxs/tokens.py
@@ -1,13 +1,13 @@
 encoding = 'utf-8'
-tokenServiceDev = str(b'tokenServiceDev', encoding)
-tokenSecretDev = bytes('tokenSecretDev', 'utf-8')
+tokenServiceDev = str(b'tokenServiceDev', encoding)#put your tokenServiceDev
+tokenSecretDev = bytes('tokenSecretDev', 'utf-8') #put your tokenSecretDev
 
 
-tokenServiceProd = str(b'tokenServiceProd', encoding)
+tokenServiceProd = str(b'tokenServiceProd', encoding)#put your tokenServiceProd
 
 
 
-tokenSecretProd = bytes('tokenSecretProd', 'utf-8')
+tokenSecretProd = bytes('tokenSecretProd', 'utf-8')#put your tokenSecretProd
 
 user = "MAIL" #put your e-mail
 password ="password"#put your password
